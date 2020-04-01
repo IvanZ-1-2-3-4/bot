94eac33809c9677b18539f05ef9e9927f3f0e7b5
Ivan Zabrodin <pyrat32@gmail.com>
2020-03-31 21:47:45 -0500

    wrote a bit of code

--- net/net.py
+++ net/net.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+# Params
+length = 5
+hiddenLayerSize = 5
+sizes = [81, hiddenLayerSize, hiddenLayerSize, hiddenLayerSize, 1]
+weights = np.random.rand(length - 1, 5, 5) # w_l^jk
+
+# Input activations
+activations = []
