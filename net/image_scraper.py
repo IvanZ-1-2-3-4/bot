6db7177e5c8d26f5221aa19038fdafe5c7688458
Ivan Zabrodin <pyrat32@gmail.com>
2020-04-03 11:20:46 -0500

    attempt at image scraper from google images

---
+++ net/image_scraper.py
@@ -0,0 +1,16 @@
+import requests as req
+from bs4 import BeautifulSoup as bs
+import wget
+
+r = req.get("https://www.google.com/search?q=impact+font+memes&tbm=isch&ved=2ahUKEwi55b-4xczoAhVCTqwKHX61DOIQ2-cCegQIABAA&oq=impact+font+memes&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADIGCAAQBRAeMgYIABAFEB4yBggAEAUQHjIGCAAQBRAeMgYIABAFEB4yBggAEAUQHjIECAAQGDoECAAQQ1CWAljTFGC8FWgCcAB4AIABZ4gB5gmSAQQxOC4xmAEAoAEBqgELZ3dzLXdpei1pbWc&sclient=img&ei=RFKHXrmzJ8KcsQX-6rKQDg&bih=937&biw=1920")
+
+
+s = bs(r.content, "html.parser")
+
+counter = 0
+for link in s.find_all("img"):
+    if (link.get("src") == None) or ("google" in link.get("src")):
+        pass
+    else:
+        image = wget.download(link.get("src"), "images/{0}.png".format(counter))
+        counter = counter + 1
