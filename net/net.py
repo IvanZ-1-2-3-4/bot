589fa14515667ac84b379967e3cb7485a0bd01d0
Ivan Zabrodin <pyrat32@gmail.com>
2020-04-03 09:45:20 -0500

    Works! net is successfully evaluated on random inputs and weights

--- net/net.py
+++ net/net.py
@@ -15,26 +15,28 @@ class Net:
         self.n = n
         self.length = length
         self.hiddenLayerSize = hiddenLayerSize
-        self.sizes = u.initialiseSizes(self.M, self.length, self.hiddenLayerSize)
+        self.layerSizes = u.initialiselayerSizes(self.M, self.length, self.hiddenLayerSize)
         # Initialise weights randomly
-        self.weights = np.random.rand(self.length - 1, 5, 5) # w_l^jk
-        self.trainingData = u.randomInputs(self.sizes, self.n)
+        self.weights = u.initialiseRandomWeights(self.layerSizes) # w_l^jk
+        self.trainingData = u.randomInputs(self.layerSizes, self.n)
 
     # One iteration of training
     def train(self):
         for i in range(len(self.trainingData["values"])):
             activations = self.evaluateNet(self.trainingData["values"][i]) # evaluate net on current training example
             print(activations[self.length - 1])
+            while True:
+                pass
 
     def evaluateNet(self, trainingExample):
-        activations = u.initialiseActivations(self.sizes, trainingExample)
+        activations = u.initialiseActivations(self.layerSizes, trainingExample)
         for l in range(1, self.length): # l = layer
-            for k in range(self.sizes[l]): # k = neuron
-                for j in range(self.sizes[l - 1]): # j = neurons in previous layer
+            for k in range(self.layerSizes[l]): # k = neuron
+                for j in range(self.layerSizes[l - 1]): # j = neurons in previous layer
                     print("l: {0}".format(l))
                     print("k: {0}".format(k))
                     print("j: {0}".format(j))
-                    activations[l][k] = activations[l][k] + activations[l - 1][j] * self.weights[l-2][k][j] # NEEDS FIX
+                    activations[l][k] = activations[l][k] + activations[l - 1][j] * self.weights[l][k][j] # NEEDS FIX
                     # Layer index is -1 for weights, as there are no weights for first layer
                 activations[l][k] = u.sigmoid(activations[l][k])
         return activations
--- net/utils.py
+++ net/utils.py
@@ -1,15 +1,26 @@
+"""
+Contains utility functions
+===========================
+
+`randomInputs(layerSizes, n)`  
+`sigmoid(x)`  
+`sigmoidPrime(x)`: derivative of sigmoid  
+`initialiseActivations(layerSizes, trainingExample)`  
+`initialiselayerSizes(M, length, hiddenLayerSize)`  
+`initialiseRandomWeights(layerSizes)`  
+"""
 import numpy as np
 
 # Make random inputs to test network before I have the data
-def randomInputs(sizes, n):
-    """ Makes random inputs to test network before I have the data, returns {values: 2d float array, 
+def randomInputs(layerSizes, n):
+    """Makes random inputs to test network before I have the data, returns {values: 2d float array,   
     labels: 2d string array, "positive" means has impact font, "negative means no impact font"}"""
     values = []
     labels = []
     for i in range(n): # Just setting the number of examples to be n-1 cause why not
         values.append([])
         labels.append([])
-        for j in range (sizes[0]):
+        for j in range (layerSizes[0]):
             # Append random new value
             values[i].append(np.random.randn())
             # Append random new label for the value
@@ -17,9 +28,7 @@ def randomInputs(sizes, n):
                 labels[i].append("positive")
             else:
                 labels[i].append("negative")
-    return {"values": values, "labels": labels}
-            
-
+    return {"values": values, "labels": labels}  
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -28,14 +37,13 @@ def sigmoidPrime(x):
     s = sigmoid(x)
     return s * (1 - s)
 
-def initialiseActivations(sizes, trainingExample):
+def initialiseActivations(layerSizes, trainingExample):
     out = [trainingExample]
-    for i in range(1, len(sizes)):
-        print(np.zeros(sizes[i]))
-        out.append(np.zeros(sizes[i]))
+    for i in range(1, len(layerSizes)):
+        out.append(np.zeros(layerSizes[i]))
     return out
 
-def initialiseSizes(M, length, hiddenLayerSize):
+def initialiselayerSizes(M, length, hiddenLayerSize):
     """Returns [M², hiddenLayerSize,...length - 2 times..., hiddenLayerSize, 1], length will """
     out = [M**2]
     for i in range(length - 2):
@@ -43,4 +51,12 @@ def initialiseSizes(M, length, hiddenLayerSize):
     out.append(1)
     return out
 
-randomInputs(initialiseSizes(16, 5, 5), 12)
+def initialiseRandomWeights(layerSizes):
+    """Returns a 3 dimensional array, with coordinates I'll call l, k and j,   
+    where l is the layer, k is the neuron in the layer, and j is the neuron in   
+    the previous layer that the weight is connected to. Array at position l=0   
+    is empty because it's the first layer."""
+    out = [None] # first layer
+    for i in range(1, len(layerSizes)):
+        out.append(np.random.randn(layerSizes[i], layerSizes[i - 1]))
+    return out
